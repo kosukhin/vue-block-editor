15fe3d25103d1576b3e0d57f6b50fadced5853fa
Eugene Kosukhin <githubkosuhin@gmail.com>
2022-11-23 11:20:41 +0300

    feat: editor

---
+++ src/shared/view/ui/BaseButton/BaseButton.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template><button></button></template>
+
+<style scoped lang="scss"></style>
---
+++ src/shared/view/ui/BaseInput/BaseInput.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>input</template>
+
+<style scoped lang="scss"></style>
